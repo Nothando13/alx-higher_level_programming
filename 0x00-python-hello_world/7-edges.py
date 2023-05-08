27fcceca2c06254741c8f410ddd9884f98c2c9fc
Phumelele Nana <nphumelele972@gmail.com>
2023-05-08 03:43:43 -0700

    Done

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+world = "Holberton"
+world_first_3 = word[:3]
+world_last_2 = world[-2:]
+middle_world = world[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {world_last_2}")
+print(f"Middle word: {middle_world}")
