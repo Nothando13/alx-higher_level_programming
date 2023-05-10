956999c756cfffcca11f1f97f84178e8b46795e5
Nothando13 <nphumelele972@gmail.com>
2023-05-10 15:27:34 +0000

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-str2 = "language that combines remarkable power with very clear syntax.\n"
+
 
 print(str)
