7e1e320d0179a5774e6c10138c42985aceda9fee
Nothando13 <nphumelele972@gmail.com>
2023-05-10 15:10:07 +0000

    7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 world = "Holberton"
-world_first_3 = word[:3]
+world_first_3 = world[:3]
 world_last_2 = world[-2:]
 middle_world = world[1:-1]
 print(f"First 3 letters: {word_first_3}")
