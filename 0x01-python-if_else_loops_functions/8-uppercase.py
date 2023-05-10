1e2dd77a738f0ab2d942b281573f664bccc84640
Nothando13 <nphumelele972@gmail.com>
2023-05-10 09:43:09 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-def to_uper(charecter):
-    if ord(character) >= 97 and ord(character) <= 122:
-        return (ord(character) -32)
-    else:
-        return ord(character)
+uppercase = _import_('8-uppercase').uppercase
 
-
-def uppercase(string):
-    string_new = ""
-    for character in string:
-        string_new += "%c" % to_uper(character)
-    print("{:s}".format(string_new))
+uppercase("best")
+uppercase("Best School 98 Battery street")
