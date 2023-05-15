5c4ca21df1becfd54751fc07dfdb32a9ec03490e
Nothando13 <nphumelele972@gmail.com>
2023-05-15 07:22:15 +0000

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    tmp_list = my_list[:]
-    if 0 <= idx < len(my_list):
-        tmp_list[idx] = element
+    listlength = len(my_list) - 1
+    if (idx < 0 or idx > listlength):
         return(tmp_list)
-<<<<<<< HEAD
-    return(my_list)
-=======
-    return(my_list_
->>>>>>> ffdd96fbd78609e11136213d5dad2e112d69e95e
+    else:
+        new_list = my_list[:]
+        new_list[idx] = element
+        return(my_list)
