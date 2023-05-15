a428cd9ac30b6c0664c2080dc428d38c5972b31b
Nothando13 <nphumelele972@gmail.com>
2023-05-15 11:54:15 +0000

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from variable_load_5 import a
+import sys
+import hidden_4
 
-def find_var():
-    print(a)
-
-if _name_ == "_main_":
-    find_var()
+if __name__ == '__main__':
+    data = dir(hidden_4)
+    for item in data:
+        if (item[:2]) != '__':
+            print("{}".format(item))
