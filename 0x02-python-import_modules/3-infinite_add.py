69bcc90bcf3e6d7651cd7435efa85fde7b6cfd6d
Phumelele Nana <nphumelele972@gmail.com>
2023-05-17 03:34:09 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
+def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= n:
+            add += int(argv[i])
+            i += 1
+        print("{:d}".format(add))
 
 if __name__ == "__main__":
-    exit()
-
-argc = len(sys.argv) - 1
-
-i = 0
-result = 0
-for arg in sys.argv:
-    if i != 0:
-        return += int(arg)
-    else:
-        i += 1
-    print("{}".format(result))    
+    import sys
+    add_arg(sys.argv)
