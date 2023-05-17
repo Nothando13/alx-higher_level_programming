915be1280b5df960770aba3e757d46b4a82d27ee
Phumelele Nana <nphumelele972@gmail.com>
2023-05-17 03:50:10 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 import sys
-if _name_ == "_main_":
-=======
+
 if __name__ == "__main__":
-    import sys
->>>>>>> ce1f2076fd923ab1b811d47413e6fb12ea4b7125
+    exit()
 
-    sys.argv.pop(0)
-    argvlength = len(sys.argv)
-
-    if (argvlength == 0:
-        print("0 arguments.")
-    elif (argvlength == 1):
-        print("1 argument:")
-        print("{:d}: {}".format(len(sys.argv[0]))
-    else:
-        print("{:d} arguments:".format(argvlength))
-        number = 1
-    for argument in sys.argv:
+argStr = "{:d} argument"
+argc = len(sys.argv) - 1
+if argc == 0:
+     argStr += 's.'
+elif argc == 1:
+    argStr += ':'
+else:
+    argStr += 'S:'
+    print(argStr.format(argc))
+    
+i = 0
+for argument in sys.argv:
+    if i != 0:
             print("{:d}:. {}".format(number, argument))
-            number += 1
+    i += 1
