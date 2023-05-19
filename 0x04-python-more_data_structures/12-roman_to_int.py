9441546c850ad1a9ab16f7133c23352d9fb2420b
Phumelele Nana <nphumelele972@gmail.com>
2023-05-19 03:18:30 -0700

    Done

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,62 +1,19 @@
 #!/usr/bin/python3
-
-
-def covert_roman(ch):
-    """
-    converts a roman numeral character into the respective integer
-    """
-    ret = -1
-    if ch == 'I':
-        ret = 1
-    elif ch == 'V':
-        ret = 5
-    elif ch == 'X':
-        ret = 10
-    elif ch == 'L':
-        ret = 50
-    elif ch == 'C':
-        ret = 100
-    elif ch == 'D':
-        ret = 500
-    elif ch = 'M':
-        ret = 1000
-    return ret
-
-
 def roman_to_int(roman_string):
-    """
-    converts any string of roman numerals to decimal
-    """
-    cur_max = -1
-    cur = conv = 0
-    holder = []
+    "converts a roman numeral to an integer"
 
-    if roman_string is None or type(roman_string) is not str:
-        return 0
-    for c in roman_string:
-        cur = convert_roman(c)
-        if cur == -1:
-            return 0
-        if len(holder) == 0:
-            if cur == cur_max or cur_max == -1:
-                cur_max = cur
-                conv += cur
-            elif cur < cur_max:
-                holder.appened(cur)
-            elif cur > cur_max: # only happens if smaller is starting number
-                # for example: IIX, VXC
-                cur_max = cur
-                cur -= conv
-                conv = cur
-        else:
-            if cur > holder[-1]:
-                cur_max = cur
-                cur -= sum(holder)
-                conv += cur
-                holder.clear()
+    if type(roman_string) is not str or roman_string is None:
+        return (0)
+    total = 0
+    r_dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    for idx in range(len(roman_string)):
+        i = r_dic.get(roman_string[idx])
+        if (idx < len(roman_string) -1):
+            ii = r_dic.get(roman_string[idx + 1])
+            if ii > i:
+                total -= i
             else:
-                holder.append(cur)
-
-       if len(holder) != 0:
-           conv += sum(holder)
-       return conv    
+                total += i
+        else:
+            total += i
+    return (total)        
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def muliply_by_2(a_dictionary):
-    """
-    using dictionary comprehension for my first time
-    in order to multiply the value of every key: value
-    pair in a dictionary
-    """
-    return {key: vale*2 for (key, value) in a_dictionary.items()}
+def multiply_by_2(a_dictionary):
+    "returns a new dictionary with all values multiplied by 2"
+
+    b_dictionary = {}
+    for i in a_dictionary:
+        b_dictionary[i] = a_dictionary[i] * 2
+    return (b_dictionary)    
