bb801927de7692d2f424025965fcccdee5e62644
Nothando13 <nphumelele972@gmail.com>
2023-05-31 22:55:41 +0000

    Done

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,7 +5,7 @@
 class Square:
    """Represent a square."""
 
-   def__init__(self, size = 0):
+   def __init__(self, size = 0):
     """Initialize a new square.
 
     Args:
@@ -28,4 +28,4 @@ class Square:
 
     def area(self):
       """Return the current area of the square."""
-      return (self.__size*self.__size)
+      return (self.__size * self.__size)
