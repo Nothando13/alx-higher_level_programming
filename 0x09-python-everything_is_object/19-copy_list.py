3ed7183afacc0a313423b9aed26b33120318d96b
Phumelele Nana <nphumelele972@gmail.com>
2023-06-07 16:58:40 -0700

    Done

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-def copy_list(l):
-    new = l[:]
-    return new
+copy_list = __import__('19-copy_list').copy_list
+
+my_list = [1, 2, 3]
+print(my_list)
+
+new_list = copy_list(my_list)
+
+print(my_list)
+print(new_list)
+
+print(new_list == my_list)
+print(new_list is my_list)
