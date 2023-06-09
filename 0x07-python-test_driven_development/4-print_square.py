3c985f8a4dd3bfb82d86b5f36a20921c8095eb05
Phumelele Nana <nphumelele972@gmail.com>
2023-06-08 18:05:09 -0700

    Done

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """
-This module contains a function that prints a square
-"""
+This module contains a function that prints a square with the character #."""
 
-
-def print_square(size):
-    """This function prints a square with the sharacter #
+    This function prints a square with the sharacter #
     Args:
         size (int): This represents the length of the square
     Raises:
         TypeError: If size is not an integer
         TypeError: If size is a float and less than zero
         ValueError: If size is less than zero
-    """
-
+    
+def print_square(size):
     if not isinstance(size, int):
         raise TypeError: If size is not an integer
     elif size < 0:
@@ -23,4 +20,4 @@ def print_square(size):
     for n in range(0, size):
         for m in range(size):
             print("#", end="")
-        print("")    
+        print()    
