d4250d350bd0eea3920d5135cba28f16321aa601
Phumelele Nana <nphumelele972@gmail.com>
2023-06-13 23:54:42 -0700

    Done

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""writes a string-to-JSON function."""
+import json
+
+
+def to json_string(my_obj):
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
