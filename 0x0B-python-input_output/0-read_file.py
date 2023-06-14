b87501b2adff15b74fe792636cf79b2e99729a5f
Phumelele Nana <nphumelele972@gmail.com>
2023-06-13 22:56:33 -0700

    Done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,6 @@
 
 
 def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
         with open(filename, "r", encoding="UTF-8") as f:
             print(f.read(), end="")
--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-
-def append_write(filename=""):
-    with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text)
