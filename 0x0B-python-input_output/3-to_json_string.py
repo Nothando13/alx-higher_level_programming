522bd7e11be58c2b0d8c3e546914d7791249c243
Phumelele Nana <nphumelele972@gmail.com>
2023-06-14 18:17:24 -0700

    Done

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,16 +5,5 @@ import json
 
 
 def to json_string(my_obj):
-    """
-    # Write a function that returns the JSON representation...
-    # ...of an object (string):
-    # VARIABLE(" "):
-    # JSON String(my_obj): To JSON string
-    # Return: Always 0. (Success)
-    """
-    """'to_json_string' function is defined with an optional parameter..
-    which represents the object to be converted to a JSON string...
-    ..The 'json.dumps()' function is used to convert the object to a...
-    JSON string...The 'dumps()' function takes an object as input and...
-    returns its JASON representation..."""
+    """ function that returns the JSON representatio of an object """
     return json.dumps(my_obj)
