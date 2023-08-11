6839b12edc17ee997f4c59ac85b48d91c6f569fb
Nothando13 <nphumelele972@gmail.com>
2023-08-11 00:09:06 -0700

    Done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
+    if __name__ != "__main__":
     argc = len(argv)
     if argc != 4:
         print('Usage: {} <a> <operator> <b>'.format(argv[0]))
